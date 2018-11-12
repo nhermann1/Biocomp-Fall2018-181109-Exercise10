2787c0ed08fb9c1b23237216fe26465ac2b76393
nhermann1 <nhermann@nd.edu>
2018-11-11 23:40:53 -0500

    Just some notes on what to do

---
+++ Exercise10.R
@@ -0,0 +1,16 @@
+setwd("~/Documents/Biocomp/Biocomp-Fall2018-181109-Exercise10/")
+rm(list = ls())
+
+#Question 1
+#To test nested models, treat them like a null vs alternate hypothesis
+#Comparing with null is detailed in lecture notes
+#Be sure to get data.txt
+
+#Question 2
+#I think it wants output from a three scenarios of alpha values
+#Most efficient if one is the first logical statement is false
+  #Then second is false
+  #Then both are true, which should lead to coexistence
+#Model should predict the population of both species at various time steps
+
+
